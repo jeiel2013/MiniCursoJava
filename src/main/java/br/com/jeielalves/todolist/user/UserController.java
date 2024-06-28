bb257b2a497b248ad460f917c92d22dee44d0833
jeiel2013 <contatojeiel2013@gmail.com>
2024-06-28 18:10:26 -0300

    fist commit

---
+++ src/main/java/br/com/jeielalves/todolist/user/UserController.java
@@ -0,0 +1,24 @@
+package br.com.jeielalves.todolist.user;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/*
+ * Modificador
+ * public
+ * private
+ * protected
+ */
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    
+    @PostMapping("/")
+    public void create(@RequestBody UserModel userModel) {
+        System.out.println(userModel.getName());
+        System.out.println(userModel.getUsername());
+        System.out.println(userModel.getPassword());
+    }
+}
